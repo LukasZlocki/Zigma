949d5cf7338576cc791eef989c5ef9b38dab334d
lzlocki <lzlocki@poczta.fm>
2023-09-20 21:10:55 +0200

    Add transformation tools

---
+++ TransformationTools/IStructureTransform.cs
@@ -0,0 +1,14 @@
+﻿namespace Zigma.TransformationTools
+{
+    internal interface IStructureTransform
+    {
+        public void ConvertCsvToListOfStringArrayStructure(List<string> dataset);
+        public List<string> ConvertListOfStringArrayStructureToCsv(List<string[]> dataset);
+
+        // GET
+        public List<string[]> GetDataset();
+
+        public void PrintDataset(int quantityOfRowsToPrint);
+
+    }
+}
