b9768db410a92c06edee6ab8c7b83d528863311b
lzlocki <lzlocki@poczta.fm>
2023-09-21 19:34:06 +0200

    Create extraction tool class to cover all extraction operations and implement csv call into it. Remove csvadapter from main zigma class

---
+++ ExtractionTools/ExtractionTool.cs
@@ -0,0 +1,23 @@
+﻿namespace Zigma.ExtractionTools
+{
+    public class ExtractionTool : ICsvAdapter
+    {
+        private CsvAdapter csvAdapter;
+
+        public ExtractionTool()
+        {
+            csvAdapter = new CsvAdapter();
+        }
+
+        /// <summary>
+        /// Save data from list os strings array (string line in format [data1, data2, data3, data4, ...) to csv file
+        /// </summary>
+        /// <param name="dataSet">Dataset to convert to csv file as list of strings array.</param>
+        /// <param name="filePath">Output file path.</param>
+        /// <param name="csvFileName">Output csv file name.</param>
+        public void SaveToCsvFile(List<string> dataSet, string filePath, string csvFileName)
+        {
+            csvAdapter.SaveToCsvFile(dataSet, filePath, csvFileName);
+        }
+    }
+}
--- Zigma.cs
+++ Zigma.cs
@@ -5,17 +5,19 @@ using Zigma.TransformationTools;
 namespace Zigma
 {
 
-    public class Zigma : ICsvAdapter, IStructureTransform, IZigmaModel
+    public class Zigma : IStructureTransform, IZigmaModel
     {
         private ZigmaModel model;
-        private CsvAdapter csvAdapter;
         private StructureTransform structTransform;
         public FileIOAdapter loader;
 
+        public ExtractionTool extractionTool;
+        
         public Zigma()
         {
+            extractionTool = new ExtractionTool();
+
             model = new ZigmaModel();
-            csvAdapter = new CsvAdapter();
             structTransform = new StructureTransform();
             loader = new FileIOAdapter();
         }
@@ -99,18 +101,6 @@ namespace Zigma
         }
         #endregion
 
-        #region CsvAdapter
-        /// <summary>
-        /// Save data from list os strings array (string line in format [data1, data2, data3, data4, ...) to csv file
-        /// </summary>
-        /// <param name="dataSet">Dataset to convert to csv file as list of strings array.</param>
-        /// <param name="filePath">Output file path.</param>
-        /// <param name="csvFileName">Output csv file name.</param>
-        public void SaveToCsvFile(List<string> dataSet, string filePath, string csvFileName)
-        {
-            csvAdapter.SaveToCsvFile(dataSet, filePath, csvFileName);
-        }
-        #endregion
 
     }
 }
