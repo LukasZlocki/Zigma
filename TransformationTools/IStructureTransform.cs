900e3b16899ac2f34d4e19e2021cc8148bf04673
lzlocki <lzlocki@poczta.fm>
2023-09-28 21:36:39 +0200

    Remove unused methods and correct interface

--- TransformationTools/IStructureTransform.cs
+++ TransformationTools/IStructureTransform.cs
@@ -2,13 +2,6 @@
 {
     internal interface IStructureTransform
     {
-        public void ConvertCsvToListOfStringArrayStructure(List<string> dataset);
-        public List<string> ConvertListOfStringArrayStructureToCsv(List<string[]> dataset);
-
-        // GET
-        public List<string[]> GetDataset();
-
-        public void PrintDataset(int quantityOfRowsToPrint);
-
+        public Zigma TransformColumnToDate(Zigma zDataset, int dateColumnNumber);
     }
 }
--- TransformationTools/StructureTransform.cs
+++ TransformationTools/StructureTransform.cs
@@ -2,79 +2,20 @@
 {
     internal class StructureTransform : IStructureTransform
     {
-        private List<string[]> MainDataset = new List<string[]>();
-
-        // ToDo:  Method moved to ZIGMA or convert to zigma during reading from csv
-        /// <summary>
-        /// Converting csv do Zigma dataset
-        /// </summary>
-        /// <param name="dataset">list of string with csv</param>
-        public void ConvertCsvToListOfStringArrayStructure(List<string> dataset)
-        {
-            List<string[]> _newDataList = new List<string[]>();
-            // ToDo: Write unit test for the class
-            foreach(string element in dataset)
-            {
-                string[] divString = element.Split(',');
-                _newDataList.Add(divString);
-            }
-            MainDataset = _newDataList;
-        }
-
-        // ToDo: Write a method here:
-        /// <summary>
-        /// Convert Zigma dataset to csv
-        /// </summary>
-        /// <param name="dataset"></param>
-        /// <returns></returns>
-        /// <exception cref="NotImplementedException"></exception>
-        public List<string> ConvertListOfStringArrayStructureToCsv(List<string[]> dataset)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Returns Zigma dataset
-        /// </summary>
-        /// <returns>Zigma dataset</returns>
-        public List<string[]> GetDataset()
-        {
-            return MainDataset;
-        }
-
-        // ToDo: Method move to ZIGMA
-        /// <summary>
-        /// Prints rows of dataset.
-        /// </summary>
-        /// <param name="quantityOfRowsToPrint">Quantity of rows to print. 0 - print all rows.</param>
-        public void PrintDataset(int quantityOfRowsToPrint)
-        {
-            int counter = 0;
-            foreach (string[] element in MainDataset)
-            {
-                if (counter == quantityOfRowsToPrint) {
-                    break;
-                }
-                string _showString = "";
-                for (int i = 0; i < element.Count(); i++)
-                {
-                    _showString = "" + _showString + ", " + "[ " + element[i] + " ]";
-                }
-                Console.WriteLine("" + _showString);
-                counter++;
-            }
-        }
-
         /// <summary>
         /// Transforming given column number to date [yyyy/mm/dd].
         /// </summary>
         /// <param name="columnNumber">Column number to transform into date.</param>
-        public void TransformColumnToDate(int columnNumber)
+        public Zigma TransformColumnToDate(Zigma zDataset, int dateColumnNumber)
         {
+            Zigma transformedDataset = new ();
+            /*
+            // ToDo: Code logic here
             double daysToAdd;
             string formattedDate;
             DateTime dateConverted;
             DateTime baseDate = new DateTime(1900, 1, 1);
+
             foreach (string[] element in MainDataset) {
 
                 string elementToConvert = element[columnNumber];
@@ -93,6 +34,9 @@
                     element[columnNumber] = formattedDate;
                 } 
             }
+            */
+            return transformedDataset;
         }
+
     }
 }
