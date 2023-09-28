8d2f0da86639335309fd44b02a5755a41f3f8584
lzlocki <lzlocki@poczta.fm>
2023-09-28 21:39:18 +0200

    Remove unused class and clean code structure

--- Zigma.cs
+++ Zigma.cs
@@ -4,63 +4,17 @@ using Zigma.TransformationTools;
 
 namespace Zigma
 {
-
     public class Zigma
     {
-        private readonly StructureTransform structTransform;
-
         public ExtractionTool extractionTool;
+        public TransformationTool transformationTool;
         public ZigmaModel zigmaModel;
 
         public Zigma()
         {
             extractionTool = new ExtractionTool();
+            transformationTool = new TransformationTool();
             zigmaModel = new ZigmaModel();
-
-            structTransform = new StructureTransform();
         }
-
-        #region StructureTransform
-        // ToDo: Method moved to ZIGMA or convert to zigma during reading from csv
-        /// <summary>
-        /// Converting csv do Zigma dataset
-        /// </summary>
-        /// <param name="dataset">list of string with csv</param>
-        public void ConvertCsvToListOfStringArrayStructure(List<string> dataset)
-        {
-            structTransform.ConvertCsvToListOfStringArrayStructure(dataset);
-        }
-
-        /// <summary>
-        /// Convert Zigma dataset to csv
-        /// </summary>
-        /// <param name="dataset">Zigma dataset</param>
-        /// <returns>List<string></returns>
-        public List<string> ConvertListOfStringArrayStructureToCsv(List<string[]> dataset)
-        {
-            return structTransform.ConvertListOfStringArrayStructureToCsv(dataset);
-        }
-
-        /// <summary>
-        /// Returns Zigma dataset
-        /// </summary>
-        /// <returns>Zigma dataset</returns>
-        public List<string[]> GetDataset()
-        {
-            return structTransform.GetDataset();
-        }
-
-        // ToDo: Method move to Zigma
-        /// <summary>
-        /// Prints rows of dataset.
-        /// </summary>
-        /// <param name="quantityOfRowsToPrint">Quantity of rows to print. 0 - print all rows.</param>
-        public void PrintDataset(int quantityOfRowsToPrint)
-        {
-            structTransform.PrintDataset(quantityOfRowsToPrint);
-        }
-        #endregion
-
-
     }
 }
