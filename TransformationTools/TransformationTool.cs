e78c560ae71d5f0bc399456a6892729df04bca65
lzlocki <lzlocki@poczta.fm>
2023-09-29 17:28:06 +0200

    Remov unused methods

--- TransformationTools/TransformationTool.cs
+++ TransformationTools/TransformationTool.cs
@@ -10,24 +10,15 @@
             transform = new StructureTransform();
         }
 
-        public void ConvertCsvToListOfStringArrayStructure(List<string> dataset)
+        /// <summary>
+        /// Transforming given column number to date to date time format [yyyy/mm/dd]
+        /// </summary>
+        /// <param name="zDataset">Zigma dataset to date transform</param>
+        /// <param name="dateColumnNumber">Column number to transform to date</param>
+        /// <returns>Zigma dataset</returns>
+        public Zigma TransformColumnToDate(Zigma zDataset, int dateColumnNumber) 
         {
-            throw new NotImplementedException();
-        }
-
-        public List<string> ConvertListOfStringArrayStructureToCsv(List<string[]> dataset)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<string[]> GetDataset()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void PrintDataset(int quantityOfRowsToPrint)
-        {
-            throw new NotImplementedException();
+            return transform.TransformColumnToDate(zDataset, dateColumnNumber);
         }
     }
 }
