d5ead96ef6f70c8d6659515a9d59b46fe16db459
lzlocki <lzlocki@poczta.fm>
2023-09-30 11:14:38 +0200

    Remove unused methiods in class and its interface

--- TransformationTools/IStructureTransform.cs
+++ TransformationTools/IStructureTransform.cs
@@ -5,9 +5,7 @@ namespace Zigma.TransformationTools
     internal interface IStructureTransform
     {
         public Zigma TransformColumnToDate(Zigma zDataset, int dateColumnNumber);
-        public ZigmaModel ColumnCreateNew(Zigma zDataset);
         public Zigma ColumnRemove(Zigma zDataset, int columnNumber);
-        public Zigma ColumnMove(Zigma zDataset, int columnNumberToMove, int newColumnPossitionNumber);
-        public Zigma ColumnSwitch(Zigma zDataset, int columnNumberToSwitch, int columnNumberSwitch);
+
     }
 }
--- TransformationTools/StructureTransform.cs
+++ TransformationTools/StructureTransform.cs
@@ -12,26 +12,10 @@ namespace Zigma.TransformationTools
 
     internal class StructureTransform : IStructureTransform
     {
-        public ZigmaModel ColumnCreateNew(Zigma zDataset)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Zigma ColumnMove(Zigma zDataset, int columnNumberToMove, int newColumnPossitionNumber)
-        {
-            throw new NotImplementedException();
-        }
-
         public Zigma ColumnRemove(Zigma zDataset, int columnNumber)
         {
             throw new NotImplementedException();
         }
-
-        public Zigma ColumnSwitch(Zigma zDataset, int columnNumberToSwitch, int columnNumberSwitch)
-        {
-            throw new NotImplementedException();
-        }
-
 
         /// <summary>
         /// Transforming given column number to date to date time format [yyyy/mm/dd]
