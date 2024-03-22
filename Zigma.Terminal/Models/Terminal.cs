e6984a8c1bbb5094c0703e68d777370135d2f59a
lzlocki <lzlocki@poczta.fm>
2024-03-22 17:33:21 +0100

    Add terminal model

---
+++ Zigma.Terminal/Models/Terminal.cs
@@ -0,0 +1,16 @@
+﻿using Zigma.Models;
+
+namespace Zigma.Terminal;
+
+public class Terminal
+{
+    List<string> HistoryCommands { get; set; }
+    List<ZigmaModel> Models { get; set; }
+
+    public Terminal()
+    {
+        HistoryCommands = new();
+        Models = new();
+    }
+
+}
