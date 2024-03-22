ddd4d706e064efad8e12b45cefd93267ac5c350f
lzlocki <lzlocki@poczta.fm>
2024-03-22 20:53:41 +0100

    Add first batch of code to process commands.

--- Zigma.Terminal/Services/ModelManipulator.cs
+++ Zigma.Terminal/Services/ModelManipulator.cs
@@ -1,12 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Zigma.Models;
 
 namespace Zigma.Terminal.Services
 {
     internal class ModelManipulator
     {
+        private ZigmaModel zModel;
+        private readonly CommandExtractor _commandExtractor;
+
+        public ModelManipulator(string commandString, ZigmaModel zModel)
+        {
+            this.zModel = zModel;
+            RunCommand(commandString);
+        }
+
+        private void RunCommand(string commandString)
+        {
+            string[] commands = _commandExtractor.ExtractCommands(commandString);
+            ProcessCommandOnLevel0(commands);
+        }
+
+        private void ProcessCommandOnLevel0(string[] commands)
+        {
+            switch (commands[0]){
+                case "extract":
+                    // perform further commands here
+                    break;
+                case "help":
+                    // perform further commands here
+                    break;
+                case "save":
+                    // perform further commands here
+                    break;
+                case "model":
+                    // perform further commands here
+                    break;
+                default:
+                    Console.WriteLine("Unknow command.");
+                    break;
+            }
+        }
+
+        private void ProcessCommandOnLevel1_extract(string[] commands)
+        {
+            switch (commands[1])
+            {
+                case "csv":
+                    // perform further commands here
+                    break;
+
+                default:
+                    Console.WriteLine("Unknow command.");
+                    break;
+            }
+        }
+
+        private void ProcessCommandOnLevel1_save(string[] commands)
+        {
+            switch (commands[1])
+            {
+                case "csv":
+                    // save data from model to csv file and path name
+                    Console.WriteLine("Saving data to csv file name: {0}, path: {1}.", commands[2], commands[3]);
+                    break;
+                default:
+                    Console.WriteLine("Unknow command.");
+                    break;
+            }
+        }
+
+        private void ProcessCommandOnLevel1_help(string[] commands)
+        {
+            Console.WriteLine("Show help here...");
+        }
+
+
+
     }
 }
