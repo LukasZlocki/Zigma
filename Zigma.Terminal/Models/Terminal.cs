569c77163b331c5b02167969724c3a734b3e4d05
lzlocki <lzlocki@poczta.fm>
2024-03-28 16:33:47 +0100

    Modify fields accesors.

--- Zigma.Terminal/Models/Terminal.cs
+++ Zigma.Terminal/Models/Terminal.cs
@@ -4,14 +4,16 @@ namespace Zigma.Terminal;
 
 public class Terminal
 {
-    List<string> HistoryCommands { get; set; }
-    List<ZigmaModel> Models { get; set; }
-    int ActiveModelPossitionInList { get; set; }
+    private List<string> HistoryCommands { get; set; }
+    private List<ZigmaModel> Models { get; set; }
+    private int ActiveModelPossitionInList = 0;
 
     public Terminal()
     {
         HistoryCommands = new();
         Models = new();
     }
+
+    pu
 
 }
