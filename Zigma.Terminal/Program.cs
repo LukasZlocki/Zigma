7dfb9d25d9a2ad2d8ad9abfaf6b198028854a54a
lzlocki <lzlocki@poczta.fm>
2024-03-28 16:17:42 +0100

    Add console sign and logic to use class with data manipulation

--- Zigma.Terminal/Program.cs
+++ Zigma.Terminal/Program.cs
@@ -1,5 +1,21 @@
-﻿Console.WriteLine("Zigma data modeler");
-Console.WriteLine("");
-Console.WriteLine("");
+﻿using Zigma.Terminal;
+using Zigma.Terminal.Services;
 
+Terminal TerminalModel = new();
+CommandExtractor ExtractCommand = new();
+ModelManipulator ManipulateModel;
 
+string Command="";
+
+Console.WriteLine("Zigma data modeler.");
+Console.WriteLine("");
+do
+{
+Console.Write("-->> ");
+Command = Console.ReadLine();
+string[] CommandWords = ExtractCommand.ExtractCommands(Command);
+
+ManipulateModel = new(CommandWords, ref TerminalModel);
+
+} while(!(Command == "exit"));
+Console.WriteLine("Zigma exited.");
